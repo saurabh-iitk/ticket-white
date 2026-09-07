260a63afe4a6e9b45729474bcd2a40b2d6bbf5db
Saurabh V <ext_saurabh.verma@uniqpay.co.in>
2026-09-07 23:58:53 +0530

    fix

--- index.php
+++ index.php
@@ -2,20 +2,39 @@
 
 /**
  * Laravel - A PHP Framework For Web Artisans
- *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
  */
 
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
-// This file allows us to emulate Apache's "mod_rewrite" functionality from the
-// built-in PHP web server. This provides a convenient way to test a Laravel
-// application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
-    return false;
+// Strip project subfolder prefix if running in a subdirectory under Apache/MAMP
+$relativeUri = preg_replace('#^/ticket-white#', '', $uri);
+$filePath = __DIR__ . '/public' . $relativeUri;
+
+if ($relativeUri !== '/' && is_file($filePath)) {
+    $mimeTypes = [
+        'css'   => 'text/css; charset=UTF-8',
+        'js'    => 'application/javascript; charset=UTF-8',
+        'png'   => 'image/png',
+        'jpg'   => 'image/jpeg',
+        'jpeg'  => 'image/jpeg',
+        'gif'   => 'image/gif',
+        'svg'   => 'image/svg+xml',
+        'ico'   => 'image/x-icon',
+        'woff'  => 'font/woff',
+        'woff2' => 'font/woff2',
+        'ttf'   => 'font/ttf',
+        'eot'   => 'application/vnd.ms-fontobject'
+    ];
+    $ext = pathinfo($filePath, PATHINFO_EXTENSION);
+    $contentType = $mimeTypes[strtolower($ext)] ?? mime_content_type($filePath);
+    
+    header('Content-Type: ' . $contentType);
+    header('Content-Length: ' . filesize($filePath));
+    readfile($filePath);
+    exit;
 }
 
 require_once __DIR__.'/public/index.php';
+
